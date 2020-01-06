99021f5ae83546c745cdaf998ba3fb2ef18d037b
Florian Wartner <florian@wartner.io>
2020-01-06 18:11:33 +0100

    Initial

---
+++ app/Commands/SyncWithBear.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Commands;
+
+use App\Services\StubService;
+use BearSync\BearNote;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Str;
+use LaravelZero\Framework\Commands\Command;
+
+class SyncWithBear extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'bear:sync';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a new post';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $noteTitle = $this->ask('What´s the title of the note?');
+        $note = BearNote::whereTitle($noteTitle)->first();
+
+        $stub = new StubService(
+            base_path('post.stub'),
+            base_path('/source/_posts/') . Str::slug($note->title, '-') . '.md'
+        );
+
+        $stub->render([
+            ':POST_TITLE:' => $note->title,
+            ':POST_DATE:' => now()->format('Y-m-d'),
+            ':POST_CONTENT:' => $note->content,
+        ]);
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
